fdc4a451f9d3192367d7b764df2a2d1583644e67
Sladuca <sladuca777@gmail.com>
2022-10-21 00:56:59 -0500

    add deal

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -16,6 +16,7 @@ use near_sdk::{
         LookupMap,
     }
 };
+use is_sorted::IsSorted;
 use barnett_smart_card_protocol::{
     BarnettSmartProtocol,
     discrete_log_cards::{
@@ -24,6 +25,7 @@ use barnett_smart_card_protocol::{
         BnMaskedCardBuf,
         BnRevealTokenWithProofBuf,
         BnZKProofKeyOwnershipBuf,
+        BnShuffleOutputBuf,
         BnCardBuf,
         BnPublicKey,
         BnPlayerSecretKey,
@@ -105,7 +107,7 @@ pub struct GameState {
 
     // game state
 
-    /// the current phase of the round - can be one of INIT, SHUFFLE, BET0, FLOP, BET1, TURN, BET2, RIVER, BET3, SHOWDOWN
+    /// current phase of the game
     pub phase: Phase,
 
     /// the current player whose turn it is
@@ -116,8 +118,11 @@ pub struct GameState {
     /// this increases by one each round
     pub dealer: usize,
 
+    /// used to check which players have revealed.
+    pub revealed_players: Vec<Option<()>>,
+
     /// the amounts each player has bet so far
-    pub bets: Vec<Balance>,
+    pub bets: Vec<BetAmount>,
 
     /// which players are still in on the round
     /// when a player folds, their corresponding `Option` is set to `None`
@@ -151,6 +156,18 @@ pub struct GameState {
     pub reveal_tokens_with_proofs: Vec<Vec<Option<BnRevealTokenWithProofBuf>>>,
 }
 
+#[derive(BorshDeserialize, BorshSerialize, Clone, Debug)]
+pub enum BetAmount {
+    /// the player is all-in
+    AllIn,
+    
+    /// the player bet some amount, isn't all-in, and hasn't folded
+    In(Balance),
+
+    /// the player folded and is out of the round with their ante still on the table
+    Folded(Balance)
+}
+
 impl GameState {
     fn new(id: GameId, player_account_ids: Vec<AccountId>, player_game_pubkeys: Vec<BnPublicKeyBuf>, pp: BnParamsBuf) -> Self {
         let num_players = player_account_ids.len();
@@ -167,10 +184,11 @@ impl GameState {
         Self {
             id,
             player_account_ids,
-            phase: Phase::INIT,
+            phase: Phase::SHUFFLE,
             turn: 0,
             dealer: 0,
-            bets: vec![0; num_players],
+            revealed_players: vec![None; num_players],
+            bets: vec![BetAmount::In(0); num_players],
             players_in: vec![Some(()); num_players],
             balances: vec![0; num_players],
             last_modified: env::block_timestamp(),
@@ -198,13 +216,37 @@ impl GameState {
     fn player_account_id(&self, player_index: usize) -> AccountId {
         self.player_account_ids[player_index].clone()
     }
+
+    fn set_deck(&mut self, deck: Vec<BnMaskedCardBuf>) {
+        self.deck = deck;
+    }
+
+    fn reset_reveal_tokens(&mut self) {
+        self.reveal_tokens_with_proofs = vec![vec![None; self.num_players()]; self.num_cards()];
+    }
+
+    fn set_reveal_token(&mut self, card_idx: usize, player_idx: usize, token: BnRevealTokenWithProofBuf) {
+        self.reveal_tokens_with_proofs[card_idx][player_idx] = Some(token);
+    }
+
+    fn set_revealed_player(&mut self, player_idx: usize) {
+        self.revealed_players[player_idx] = Some(());
+    }
+
+    fn reset_revealed_players(&mut self) {
+        self.revealed_players = vec![None; self.num_players()];
+    }
+
+    fn all_players_revealed(&self) -> bool {
+        self.revealed_players.iter().all(|x| x.is_some())
+    }
 }
 
 
 #[derive(BorshDeserialize, BorshSerialize)]
 pub enum Phase {
-    INIT,
     SHUFFLE,
+    DEAL,
     BET0,
     FLOP,
     BET1,
@@ -340,6 +382,118 @@ impl Contract {
             _ => panic!("game is no longer accepting players")
         }
     }
+
+    // init the deck - game creator calls this once at the beginning
+    // TODO (later): verify the masking proofs. There's a lot of them and it's probably a pain so I'm skipping it for now (oopsies)
+    pub fn init_deck(&mut self, game_id: GameId, deck: Vec<BnMaskedCardBuf>) {
+        assert!(self.games.contains_key(&game_id), "game does not exist");
+
+        let mut game = self.games.get(&game_id).unwrap();
+        match game {
+            Game::InProgress(ref mut state) => {
+                let account_id = env::predecessor_account_id();
+                assert!(state.player_account_ids[0] == account_id, "only the creator can init the deck");
+                assert!(state.deck.len() == 0, "deck must not have been initialized yet");
+                assert!(deck.len() == 52, "deck must have 52 cards");
+
+                state.reset_reveal_tokens();
+                state.set_deck(deck)
+            },
+            _ => panic!("game is not in progress")
+        }
+    }
+
+    // shuffle the deck - each player calls this going around one at a time in turn order - the dealer calls this first
+    pub fn shuffle_deck(&mut self, game_id: GameId, shuffle: BnShuffleOutputBuf) {
+        assert!(self.games.contains_key(&game_id), "game does not exist");
+
+        let mut game = self.games.get(&game_id).unwrap();
+        match game {
+            Game::InProgress(ref mut state) => {
+                let account_id = env::predecessor_account_id();
+                assert!(state.player_account_ids.contains(&account_id), "only players can shuffle the deck");
+
+                let player_index = state.player_account_ids.iter().position(|id| id == &account_id).unwrap();
+                assert!(state.turn == player_index, "it is not your turn to shuffle the deck");
+
+                let (deck, proof) = shuffle.deserialize().expect("failed to deserialize shuffle");
+                let pp = self.trusted_setup_params.deserialize().expect("failed to deserialize trusted setup params");
+                let aggregate_pubkey = state.aggregate_pubkey.deserialize().expect("failed to deserialize aggregate pubkey");
+                let mut old_deck = Vec::new();
+                for card in state.deck.iter() {
+                    old_deck.push(card.deserialize().expect("failed to deserialize card"));
+                }
+
+                BnCardProtocol::verify_shuffle(&pp, &aggregate_pubkey, &old_deck, &deck, &proof).expect("failed to verify shuffle proof");
+
+                let mut shuffled_deck = Vec::new();
+                for card in deck {
+                    let card = BnMaskedCardBuf::serialize(card).expect("failed to serialize masked card");
+                    shuffled_deck.push(card);
+                }
+                state.set_deck(shuffled_deck);
+                state.reset_reveal_tokens();
+                state.turn = (state.turn + 1) % state.num_players();
+
+                if state.turn == state.dealer {
+                    state.phase = Phase::DEAL;
+                }
+            },
+            _ => panic!("game is not in progress")
+        }
+    }
+
+    // deal everyone their two cards - each player has to call (any order) this with their reveal tokens calculated client-side.
+    pub fn deal(&mut self, game_id: GameId, card_indices: Vec<usize>, reveal_tokens_with_proofs: Vec<BnRevealTokenWithProofBuf>) {
+        assert!(self.games.contains_key(&game_id), "game does not exist");
+
+        let mut game = self.games.get(&game_id).unwrap();
+        match game {
+            Game::InProgress(ref mut state) => {
+                assert!(matches!(state.phase, Phase::DEAL), "game is not in the deal phase");
+                let account_id = env::predecessor_account_id();
+                assert!(state.player_account_ids.contains(&account_id), "only players can deal");
+
+                let player_index = state.player_account_ids.iter().position(|id| id == &account_id).unwrap();
+
+                // player at idx i gets revealed 2*i, 2*i+1
+                // => player at idx i should reveal every card but those cards
+                // TODO checks later
+                // assert!(card_indices.len() == reveal_tokens_with_proofs.len(), "card indices and reveal tokens with proofs should have same len");
+                // assert!(card_indices.iter().is_sorted(), "card indices must be sorted");
+                // let expected_card_indices = (0..2*state.num_players()).filter(|&i| player_index * 2 != i && player_index * 2 + 1 != i).collect::<Vec<usize>>();
+                // assert!(card_indices.len() == expected_card_indices.len(), "you must reveal the correct number of cards");
+                // assert!(card_indices.iter().all(|i| expected_card_indices.contains(i)), "you must reveal the correct cards");
+                // let prededup_len = card_indices.len();
+                // card_indices.dedup();
+                // assert!(card_indices.len() == prededup_len, "card indices cannot have duplicates");
+
+                let pp = self.trusted_setup_params.deserialize().expect("failed to deserialize trusted setup params");
+                let pk = state.player_game_pubkeys[player_index].deserialize().expect("failed to deserialize player pubkey");
+
+                for (card_idx, reveal_token_with_proof) in card_indices.into_iter().zip(reveal_tokens_with_proofs) {
+                    let (reveal_token, proof) = reveal_token_with_proof.deserialize().expect("failed to deserialize reveal token with proof");
+                    let masked_card = state.deck[card_idx].deserialize().expect("failed to deserialize masked card");
+                    BnCardProtocol::verify_reveal(&pp, &pk, &reveal_token, &masked_card, &proof).expect("failed to verify reveal token proof");
+                    state.set_reveal_token(card_idx, player_index, reveal_token_with_proof);
+                }
+
+                state.set_revealed_player(player_index);
+
+                if state.all_players_revealed() {
+                    state.phase = Phase::BET0;
+                }
+            },
+            _ => panic!("game is not in progress")
+        }
+    }
+
+
+    // place bet - players call this in turn order until the betting is done. this is only called during the bet phases
+
+    // reveal cards - each player has to call this (any order) with their reveal tokens calculated client side. number of cards revealed depends on the phase
+
+
 }
 
 /*
@@ -348,25 +502,6 @@ impl Contract {
  */
 #[cfg(test)]
 mod tests {
-    use super::*;
-
-    // #[test]
-    // fn get_default_greeting() {
-    //     let contract = Contract::default();
-    //     // this test did not call set_greeting so should return the default "Hello" greeting
-    //     assert_eq!(
-    //         contract.get_greeting(),
-    //         "Hello".to_string()
-    //     );
-    // }
-
-    // #[test]
-    // fn set_then_get_greeting() {
-    //     let mut contract = Contract::default();
-    //     contract.set_greeting("howdy".to_string());
-    //     assert_eq!(
-    //         contract.get_greeting(),
-    //         "howdy".to_string()
-    //     );
-    // }
+    // TODO
 }
+
