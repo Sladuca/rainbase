5cbf61e34780cd41bc5a1c5020f2b735968545f4
Sladuca <sladuca777@gmail.com>
2022-10-20 23:54:20 -0500

    reduce deserialization gas by 20x so it actually runs

--- contract/deploy.sh
+++ contract/deploy.sh
@@ -12,7 +12,8 @@ cargo run --example trusted_setup
 PARAMS_JSON=`cat params.json`
 
 echo ">> Deploying contract"
+rm -rf neardev
 ARGS="{ \"trusted_setup_params\": $PARAMS_JSON }"
 near dev-deploy --wasmFile ./target/wasm32-unknown-unknown/release/rainbase_contract.wasm
 DEV_ACCOUNT=`cat neardev/dev-account`
-near call $DEV_ACCOUNT init "'$ARGS'" --accountId $DEV_ACCOUNT
+near call $DEV_ACCOUNT init "'$ARGS'" --accountId $DEV_ACCOUNT --gas 300000000000000
